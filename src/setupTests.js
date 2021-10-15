e40cfe5bbd3dd82497ef20867813ed2356b1750e
Joy Meredith <joy@virtualpowersystems.com>
2021-10-14 22:12:09 -0400

    mock service worker setup

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,3 +3,14 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+
+import {server} from './mocks/server.js'
+// establish API mocking before all tests
+// any network requests that come through, route them to mock service worker
+// instead of the actual network
+beforeAll(()=> server.listen())
+// reset any request handlers that may be added during the tests,
+// so they don't affect other tests.
+afterEach(() => server.resetHandlers())
+// clean up after the tests are finished
+afterAll(()=> server.close())
