6841c2dd7187b80df7a90bcf4963bef478e04075
Joy Meredith <joy@virtualpowersystems.com>
2021-10-17 21:43:04 -0400

    topping checkbox tests

--- src/pages/entry/test/totalUpdates.test.jsx
+++ src/pages/entry/test/totalUpdates.test.jsx
@@ -1,10 +1,10 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen } from "../../../test-utils/testing-library-utils";
 import userEvent from "@testing-library/user-event";
 import Options from "../Options";
 import { OrderDetailsProvider } from "../../../contexts/OrderDetails";
 
-test("update scopp subtotal when scoops change", async () => {
-  render(<Options optionType="scoops" />, { wrapper: OrderDetailsProvider });
+test("update scoop subtotal when scoops change", async () => {
+  render(<Options optionType="scoops" />);
 
   // make sure total starts out $0.00
   // exact false because part of the entire element
@@ -27,4 +27,34 @@ test("update scopp subtotal when scoops change", async () => {
   userEvent.clear(chocolateInput); // clears text element, don't know where it will put the cursor etc
   userEvent.type(chocolateInput, "2");
   expect(scoopSubtotal).toHaveTextContent("6.00");
+});
+
+test("update toppings subtotal when toppings change", async () => {
+  render(<Options optionType="toppings" />);
+
+  // make sure total starts out $0.00
+  // exact false because part of the entire element
+  // our expect will be the rest
+  const toppingsSubtotal = screen.getByText("Toppings total: $", {
+    exact: false,
+  });
+  expect(toppingsSubtotal).toHaveTextContent("0.00");
+
+  // add cherries and check subtotal
+  const cherriesCheckbox = await screen.findByRole("checkbox", {
+    name: "Cherries",
+  });
+  userEvent.click(cherriesCheckbox); // clears text element, don't know where it will put the cursor etc
+  expect(toppingsSubtotal).toHaveTextContent("1.50");
+
+  // add hot fudge and check subtotal
+  const hotFudgeCheckbox = await screen.findByRole("checkbox", {
+    name: "Hot fudge",
+  });
+  userEvent.click(hotFudgeCheckbox); // clears text element, don't know where it will put the cursor etc
+  expect(toppingsSubtotal).toHaveTextContent("3.00");
+
+  // remove hot fudge and check subtoal
+  userEvent.click(hotFudgeCheckbox);
+  expect(toppingsSubtotal).toHaveTextContent("1.50");
 });
