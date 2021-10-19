0e2b50a6779dab987455e3977380d2ebf3663947
Joy Meredith <joy@virtualpowersystems.com>
2021-10-18 22:18:38 -0400

    full orderPhase tests

--- src/pages/entry/OrderEntry.jsx
+++ src/pages/entry/OrderEntry.jsx
@@ -1,17 +1,17 @@
-import React from "react";
-import { useOrderDetails } from "../../contexts/OrderDetails";
+import Button from "react-bootstrap/Button";
 import Options from "./Options";
+import { useOrderDetails } from "../../contexts/OrderDetails";
 
-function OrderEntry() {
+export default function OrderEntry({ setOrderPhase }) {
   const [orderDetails] = useOrderDetails();
+
   return (
     <div>
       <h1>Design Your Sundae!</h1>
       <Options optionType="scoops" />
       <Options optionType="toppings" />
       <h2>Grand total: {orderDetails.totals.grandTotal}</h2>
+      <Button onClick={() => setOrderPhase("review")}>Order Sundae!</Button>
     </div>
   );
 }
-
-export default OrderEntry;
